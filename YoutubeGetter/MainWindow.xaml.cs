409f7931806b8f24432b612bafc8eb9deb7bfd68
Miguel Alho <alho@miguelalho.com>
2014-06-15 14:29:34 +0100

    Added basic project files
    Added files for a basic working copy of the app. Accepts an address and
    downloads audio.

---
+++ YoutubeGetter/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YoutubeExtractor;
+
+namespace YoutubeGetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string savepath = @"C:\Download\";
+        
+        public MainWindow()
+        {
+            if (!Directory.Exists(savepath))
+                Directory.CreateDirectory(savepath);
+        }
+
+        private void Button_Click(object formsender, RoutedEventArgs e)
+        {
+            // Our test youtube link
+            string link = YoutubeLink.Text;
+
+            /*
+             * Get the available video formats.
+             * We'll work with them in the video and audio download examples.
+             */
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            /*
+             * We want the first extractable video with the highest audio quality.
+             */
+            VideoInfo video = videoInfos
+                .Where(info => info.CanExtractAudio)
+                .OrderByDescending(info => info.AudioBitrate)
+                .First();
+
+            /*
+             * Create the audio downloader.
+             * The first argument is the video where the audio should be extracted from.
+             * The second argument is the path to save the audio file.
+             */
+            var audioDownloader = new AudioDownloader(video, System.IO.Path.Combine(savepath, video.Title + video.AudioExtension));
+
+            // Register the progress events. We treat the download progress as 85% of the progress and the extraction progress only as 15% of the progress,
+            // because the download will take much longer than the audio extraction.
+            audioDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 0.85);
+            audioDownloader.AudioExtractionProgressChanged += (sender, args) => Console.WriteLine(85 + args.ProgressPercentage * 0.15);
+
+            /*
+             * Execute the audio downloader.
+             * For GUI applications note, that this method runs synchronously.
+             */
+            audioDownloader.Execute();
+
+        }
+    }
+}
